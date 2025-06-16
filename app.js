6cd2242f1136a5a100e28719914b239c7fc2de7e
sahil22112004 <sahilkondal128@gmail.com>
2025-06-17 00:09:56 +0530

    again cred

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN || process.env.LOCAL_CORS_ORIGIN ,
-    credentials: true,
+    credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
